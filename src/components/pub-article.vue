4f98c2a99e67e1c08c080c648149dafde13a3a9b
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-08-22 03:44:53 -0500

    feat: Add pub-articles component for display articles by search

---
+++ src/components/pub-article.vue
@@ -0,0 +1,82 @@
+<template>
+  <article class="sc-article">
+
+    <div class="num-article">
+      <div class="check"></div>
+      <span>1</span>
+    </div>
+
+    <div class="info-article">
+      <h5>Scientific Workflows for bibliiometric</h5>
+      <h6>
+        <a href="#">Guler, AT</a> and
+        <a href="#">Painblad, M</a>
+      </h6>
+      <span>May 2016 | <a href="#">SCIENTOMETRICS</a>107 (2), pp.385-398</span>
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+        rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+        consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+        dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+        incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+        vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui
+        dolorem eum fugiat quo voluptas nulla pariatur?
+      </p>
+      <span>
+              <a href="#">Free full text from publisher</a> |
+              <a href="#">Google Scholar Lookup</a>
+            </span>
+    </div>
+
+    <div class="bb-data-article">
+      <div class="metadata-art">
+        <div class="metadata-type">
+          <h2>Q2</h2>
+          <i class='bx bx-link-external'></i>
+        </div>
+
+        <div>
+          <h5>Category Quartile</h5>
+          <h6>From SJR 2018</h6>
+        </div>
+        <hr>
+      </div>
+      <div class="metadata-art">
+        <div class="metadata-type">
+          <h2>3.238</h2>
+          <i class='bx bx-link-external'></i>
+        </div>
+
+        <div>
+          <h5>Journal Impact Factor</h5>
+          <h6>From SJR 2018</h6>
+        </div>
+        <hr>
+      </div>
+      <div class="metadata-art">
+        <div class="metadata-type">
+          <h2>116</h2>
+          <i class='bx bx-link-external'></i>
+        </div>
+
+        <div>
+          <h5>H-Index</h5>
+          <h6>From SJR 2018</h6>
+        </div>
+        <hr>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "pub-article"
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/search-results-page/articles-search-rs";
+</style>
