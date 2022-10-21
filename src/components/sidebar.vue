251d3141ea84839502a0259183a23bb2f5b2a641
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2022-10-21 18:47:46 -0500

    Centered sidebar icons.

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -3,37 +3,37 @@
 		<div
 			class="overflow-y-auto overflow-x-auto h-full py-3 px-1 bg-main rounded-r-lg"
 		>
-			<ul>
-				<li class="mb-2">
-					<div
-						class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100 hover:cursor-pointer"
-						@click="goToSearch()"
-					>
-						<Icon icon="bx-search-alt-2" width="36" height="36" />
-						<span class="text-xs flex-1 whitespace-nowrap">Search</span>
-					</div>
-				</li>
-				<li class="mb-2">
-					<router-link
-						class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100"
-						to="/compare"
-						><Icon icon="mdi:file-compare" width="36" height="36" />
-						<span class="text-xs flex-1 whitespace-nowrap"
-							>Compare</span
-						></router-link
-					>
-				</li>
-				<li class="mb-2">
-					<router-link
-						class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100"
-						to="/list-collection"
-						><Icon icon="mdi:list-box-outline" width="36" height="36" />
-						<span class="text-xs flex-1 whitespace-nowrap"
-							>Collections</span
-						></router-link
-					>
-				</li>
-			</ul>
+      <ul>
+        <li class="mt-2 mb-6 ms-n6 w-24">
+          <div
+              class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100 hover:cursor-pointer"
+              @click="goToSearch()"
+          >
+            <Icon icon="bx-search-alt-2" class="w-10 h-10"/>
+            <span class="text-xs flex-1 whitespace-nowrap mt-3">Search</span>
+          </div>
+        </li>
+        <li class="mb-6 ms-n6 w-24">
+          <router-link
+              class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100"
+              to="/compare"
+          ><Icon icon="mdi:file-compare" class="w-10 h-10"/>
+            <span class="text-xs flex-1 whitespace-nowrap mt-3"
+            >Compare</span
+            ></router-link
+          >
+        </li>
+        <li class="mb-6 ms-n6 w-24">
+          <router-link
+              class="flex items-center justify-center flex-col p-2 text-base font-normal text-white rounded-lg hover:bg-gray-100"
+              to="/list-collection"
+          ><Icon icon="mdi:list-box-outline" class="w-10 h-10"/>
+            <span class="text-xs flex-1 whitespace-nowrap mt-3"
+            >Collections</span
+            ></router-link
+          >
+        </li>
+      </ul>
 		</div>
 	</aside>
 </template>
